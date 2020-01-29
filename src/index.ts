d76a527935dd42a4b1f1b86ccd49f834951e7a46
Dobes Vandermeer <dobesv@gmail.com>
2020-01-28 17:06:07 -0800

    Export parameter gatherer types & classes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./PlanOutCode";
 export * from "./PlanOutExperiment";
 export * from "./PlanOutInterpreter";
+export * from "./PlanOutParameterGatherer";
 import experiment from "./experiment";
 import { parse as compile } from "./parser";
 import execute from "./execute";
